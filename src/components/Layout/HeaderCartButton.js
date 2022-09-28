9967f3e46a19c29152e00a46ece7f1ef9158bf8b
aldorinoadhisasmita <aldorinoadhisasmita@gmail.com>
2022-09-28 13:44:09 +0700

    Section 11 Done

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
-import styles from './HeaderCartButton.module.css'
+import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnBump, setBtnBump] = useState(false);
+
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+
+  const btnClasses = `${styles.button} ${btnBump ? styles.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={styles.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>
-        3
-      </span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
