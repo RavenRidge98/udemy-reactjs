4e7e5fc9e45c18746262779a00f604b085fc26f5
Aldorino Adhisasmita Yandoyo <55988714+RavenRidge98@users.noreply.github.com>
2022-10-05 18:26:00 +0700

    Handling Errors

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,17 +1,7 @@
-<<<<<<< Updated upstream
-import './Card.css';
-
-function Card(props) {
-  const classes = 'card ' + props.className;
-  
-  return <div className={classes}>{props.children}</div>;
-}
-=======
 import classes from './Card.module.css';
 
 const Card = props => {
   return <div className={classes.card}>{props.children}</div>
 };
->>>>>>> Stashed changes
 
 export default Card;
