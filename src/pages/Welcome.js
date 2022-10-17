13877bb574937dd3cf99ee460d802067faff4c0c
aldorinoadhisasmita <aldorinoadhisasmita@gmail.com>
2022-10-17 12:41:30 +0700

    Routing basics done

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,11 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
   return (
     <section>
-      <h1>The Welcome Page</h1>;
+      <h1>The Welcome Page</h1><Route path='/welcome/new-user'>
+        <p>Welcome, New User</p>
+      </Route>
     </section>
   );
 };
